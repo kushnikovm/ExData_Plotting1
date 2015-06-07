01dce29d0e163fb99f06cf07da8703573d115b4a
MIKEK <kuhsnikov.m@gmail.com>
2015-06-07 22:23:47 +0300

    plot 3 and 4

---
+++ plot4.R
@@ -0,0 +1,32 @@
+## reading data
+data<-read.csv("./household_power_consumption.txt",header = TRUE, 
+               na.strings = "?", sep = ";")
+## formatting Date and Time from Factor to Date
+data$Date<-as.Date( x = data$Date, format = "%d/%m/%Y")
+## searching rows only for 2007-02-01 and 2007-02-02
+pldata <- data[grep("2007-02-0[12]",data$Date),]
+## setting Date and Time together (to the list) 
+dateTime   <- as.POSIXlt(paste(as.Date(pldata$Date, format="%d/%m/%Y"), 
+                               pldata$Time, sep=" "))
+## solving problem with language of days names
+backup_locale <- Sys.getlocale('LC_TIME')
+Sys.setlocale('LC_TIME', 'C')
+## plotting
+par(mfrow = c(2,2))
+plot(dateTime,pldata$Global_active_power, type = "l", xlab = "", 
+     ylab = "Global Active Power", col = "black" )
+plot(dateTime,pldata$Voltage, type = "l", xlab = "datetime", 
+     ylab = "Voltage", col = "black" )
+plot(dateTime,pldata$Sub_metering_1, type = "l", xlab = "", 
+     ylab = "Energy sub metering", col = "black" )
+lines(dateTime,pldata$Sub_metering_2, col = "red"  )
+lines(dateTime,pldata$Sub_metering_3, col = "blue" )
+legend("topright", legend = c("Sub_metering_1","Sub_metering_2"
+       ,"Sub_metering_3" ), lty = c(1,1,1) , bty="n", 
+       col = c("black", "red", "blue"), cex = 0.7 )
+plot(dateTime,pldata$Global_reactive_power, type = "l",xlab = "datetime", 
+     ylab = "Global Reactive Power", col = "black" )
+## saving to png file
+dev.copy(png, file = "./plot4.png", width = 480, height = 480)
+dev.off()
+Sys.setlocale('LC_TIME', backup_locale)
